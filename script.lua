85c59909a7c8b2065e0be38e761ffb4e3125695a
Salim Perchy <salim.perchy@gmail.com>
2021-06-03 14:22:59 +0200

    added comments to script

--- script.lua
+++ script.lua
@@ -1,3 +1,9 @@
+-- Support shape based on type
+-- A --> anchors: between an anchor voxel (white) and the object
+-- B --> bars: horizontal bars
+-- D --> diagonals: offsets and anchor merges
+-- F --> for the anchor pillars that touch the object, they signal the connection between the pillar and the object (I have created a new voxel ID for this)
+-- P --> pillars: vertical bars
 function support_shape(x0,y0,z0,x1,y1,z1,t)
   v0 = v(x0,y0,z0)
   v1 = v(x1,y1,z1)
@@ -22,18 +28,18 @@ end
 -- Load model
 modelfile = 'model.stl'
 model = load(Path..modelfile)
-bx = bbox(model)
+bx = bbox(model) -- bounding box
 
--- Load model bounding box extent
+-- Load model bounding box extent (output from voxelizer)
 transfile = 'out.slab.info'
 trans = io.open(Path..transfile)
 line = trans:read()
-ex,ey,ez = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*)")
-line = trans:read()
-vx,vy,vz = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*)")
-trans:close()
+ex,ey,ez = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*)") -- bounding box extent
 ex = tonumber(ex) ey = tonumber(ey) ez = tonumber(ez)
+line = trans:read()
+vx,vy,vz = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*)") -- voxel space exent
 vx = tonumber(vx) vy = tonumber(vy) vz = tonumber(vz)
+trans:close()
 vdx = ex / vx vdy = ey / vy vdz = ez / vz -- voxel size
 
 -- Read segments
@@ -44,18 +50,25 @@ segmentsD = {}
 segmentsF = {}
 segmentsP = {}
 for line in io.lines(Path..segmentsfile) do
-  x0,y0,z0,x1,y1,z1,t = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%u)")
+  x0,y0,z0,x1,y1,z1,t = string.match(line, "(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%d+.?%d*),(%u)") -- read segment from file
   --print(x0..','..y0..','..z0..','..x1..','..y1..','..z1..','..t)
   x0 = tonumber(x0) y0 = tonumber(y0) z0 = tonumber(z0)
   x1 = tonumber(x1) y1 = tonumber(y1) z1 = tonumber(z1)
-  x0 = 1.0 - x0 x1 = 1.0 - x1
-  y0 = 1.0 - y0 y1 = 1.0 - y1
-  offset = 0.0
-  local shape = translate(bx:min_corner()) * 
+  x0 = 1.0 - x0 x1 = 1.0 - x1 -- flip X axis (due to MagicaVoxel weirdness)
+  y0 = 1.0 - y0 y1 = 1.0 - y1 -- flip Y axis (due to MagicaVoxel weirdness)
+  offset = -0.0
+  local shape = translate(bx:min_corner()) * -- position within model space
     support_shape(
-	ex * x0 + offset * vdx, ey * y0 - offset * vdy, ez * z0 - 0.0 * vdz,
-	ex * x1 - offset * vdx, ey * y1 - offset * vdy, ez * z1 - 0.0 * vdz,
-	t)
+	-- p0
+	ex * x0 + offset * vdx, -- convert from normalized voxel space to model space and add offset in # of voxel size
+	ey * y0 + offset * vdy,
+	ez * z0 + 0.0 * vdz,
+	-- p1
+	ex * x1 + offset * vdx,
+	ey * y1 + offset * vdy,
+	ez * z1 + 0.0 * vdz,
+	-- type
+	t
 	
   if (t == 'A') then
     segmentsA[#segmentsA + 1] = shape
@@ -72,7 +85,7 @@ for line in io.lines(Path..segmentsfile) do
   end
 end
 
--- Output shape
+-- Output shape and supports
 emit(model,0)
 emit(union(segmentsA),1)
 emit(union(segmentsB),2)
@@ -80,9 +93,12 @@ emit(union(segmentsD),3)
 emit(union(segmentsF),4)
 emit(union(segmentsP),5)
 
-boundingbox = translate(bx:min_corner()) * ocube(ex,ey,ez * 0.999)
+-- Debug shapes for purposes of checking aligment with normalized voxel segments and model extent from voxelizer
+boundingboxV = translate(bx:min_corner()) * ocube(ex,ey,ez * 0.999)
+boundingboxM = translate(bx:min_corner()) * ocube(bx:extent().x,bx:extent().y,bx:extent().z * 0.999)
 barX = translate(bx:min_corner()) * ocube(ex,1,1)
 barY = translate(bx:min_corner()) * ocube(1,ey,1)
 barZ = translate(bx:min_corner()) * ocube(1,1,ez)
 --emit(union{barX, barY, barZ})
---emit(boundingbox)
+--emit(boundingboxV)
+--emit(boundingboxM)
